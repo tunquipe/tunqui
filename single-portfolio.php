9006cc8669e6098e01a5d9e30ecd80842bc6df56
Alex Aragon <aragcar@gmail.com>
2022-04-28 13:08:43 -0500

    porftolio single

--- single-portfolio.php
+++ single-portfolio.php
@@ -2,26 +2,26 @@
 
 get_header();
 $idPost = get_the_ID();
+
+$gallery = get_field('photo_gallery');
+$categories = get_the_terms($idPost,'portfolio_category');
+$listCategories = null;
+foreach($categories as $category){
+    $listCategories .= $category->name.' - ';
+}
+                
 ?>
 <?php while(have_posts()): the_post(); ?>
 
     <!-- ======= Hero Section ======= -->
-    <section id="hero-page" class="d-flex align-items-center">
+    <section id="hero-portfolio" class="d-flex align-items-center">
         <div class="container">
             <div class="row align-items-start justify-content-center">
-                <div class="col-12 col-lg-7 d-flex flex-column justify-content-center pt-2 pt-lg-0 order-2 order-lg-1">
+                <div class="col-12 col-lg-12 d-flex flex-column justify-content-center pt-2 pt-lg-0 order-2 order-lg-1">
                     <div class="page-title">
                         <h1><?php the_title(); ?></h1>
                         <p><?php echo get_post_meta($idPost,'sub_title', true); ?></p>
                     </div>
-                </div>
-                <div class="col-12 col-lg-5 order-1 order-lg-2 hero-img">
-                    <?php if ( has_post_thumbnail() ) :
-                        $featured_image = get_the_post_thumbnail_url();
-
-                        ?>
-                        <img class="img-fluid" src="<?php echo $featured_image; ?>" />
-                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -31,7 +31,48 @@ $idPost = get_the_ID();
         <div class="container">
             <section id="page-home" class="page-home">
                 <div class="container">
+
+                        <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="container">
+          <div class="row gy-4">
+
+                <div class="col-lg-8">
+                    <div class="portfolio-details-slider swiper">
+                    <div class="swiper-wrapper align-items-center">
+                        <?php foreach ($gallery as $image) : ?>
+                            <div class="swiper-slide">
+                            <img src="<?php echo $image['full_image_url']; ?>" alt="">
+                            </div>
+                        <?php endforeach; ?>
+
+                    </div>
+                    <div class="swiper-pagination"></div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4">
+                    <div class="portfolio-info">
+                    <h3>Información del Proyecto</h3>
+                    <ul>
+                        <li><strong>Categoría</strong>: <?php echo $listCategories; ?></li>
+                        <li><strong>Cliente</strong>: <?php echo get_field('customer'); ?></li>
+                        <li><strong>Fecha del proyecto</strong>: <?php echo get_field('project_date'); ?></li>
+                        <li><strong>URL del proyecto</strong>: <a target="_blank"  href="<?php echo get_field('project_url'); ?>"><?php echo get_field('project_url'); ?></a></li>
+                    </ul>
+                    </div>
+                    <div class="portfolio-description">
+                    <h2><?php the_title(); ?></h2>
                     <?php the_content(); ?>
+                    </div>
+                </div>
+
+                </div>
+
+            </div>
+            </section><!-- End Portfolio Details Section -->
+
+                    
                 </div>
             </section>
         </div>
