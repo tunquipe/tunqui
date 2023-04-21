376c42c159080f6c3f8bd5b4768574f6f341a5c2
Alex Aragon <aragcar@gmail.com>
2023-04-21 15:41:59 -0500

    fix theme

--- home.php
+++ home.php
@@ -16,37 +16,29 @@ $sliders = tunqui_get_slider();
         <div class="container">
             <div class="row">
                 <div id="carousel_slider" class="carousel slide" data-bs-ride="carousel">
-                    <div class="stripe">
-
-                    </div>
-                    <div class="carousel-indicators">
-                        <?php $counter = -1; ?>
-                        <?php foreach ($sliders as $item) : ?>
-                            <?php $counter++; ?>
-                            <button type="button" data-bs-target="#carousel_slider" data-bs-slide-to="<?php echo $counter; ?>" class="<?php echo $item['active'] ?>" aria-label="Slide <?php echo $counter; ?>"></button>
-                        <?php endforeach; ?>
-                    </div>
+                    <!--<div class="stripe">
+                    </div>-->
                     <div class="carousel-inner">
                         <?php $counter = -1; ?>
                         <?php foreach ($sliders as $item) : ?>
                             <?php $counter++; ?>
                             <div class="carousel-item <?php echo $item['active'] ?>">
                                 <div class="row">
-                                    <div class="col-lg-6 d-flex flex-column justify-content-center pt-2 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+                                    <div class="col-lg-7 d-flex flex-column justify-content-center pt-2 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
                                         <div class="item-content">
                                             <h2><?php echo $item['slider_sub_title']; ?></h2>
                                             <h1><?php echo $item['slider_title']; ?></h1>
                                             <div class="d-grid gap-2 d-md-flex justify-content-center justify-content-lg-start">
                                                 <?php if ($item['slider_url_target']) : ?>
                                                     <a href="<?php echo $item['slider_url_target']; ?>" class="btn-get-started scrollto">
-                                                        <?php echo $item['slider_text_button']; ?> <i class='bx bxs-arrow-from-left'></i>
+                                                       <span><?php echo $item['slider_text_button']; ?></span> <i class="bx bx-right-arrow-alt bx-sm"></i>
                                                     </a>
                                                 <?php endif; ?>
                                             </div>
                                         </div>
 
                                     </div>
-                                    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                                    <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
                                         <img class="img-fluid" src="<?php echo $item['slider_img_full'][0]; ?>" alt="">
                                     </div>
                                 </div>
@@ -67,51 +59,10 @@ $sliders = tunqui_get_slider();
     </section><!-- End Hero -->
 
 <?php endif; ?>
-
-<!--<section id="courses" class="section-courses">
-    <div class="container">
-        <div class="row">
-            <div class="list-courses owl-carousel owl-theme">
-            <?php /*foreach ($courses as $course) : */?>
-                
-                    <div class="course-item hvr-float">
-                        <div class="course-img">
-                            <?php /*echo getPriceCourse($course['id']); */?>
-                            <img src="<?php /*echo $course['course_image_full'][0]; */?>" class="img-fluid" alt="">
-                        </div>
-                        <div class="course-info">
-                            <ul class="tutor-ul tutor-d-flex">
-                                <li class="tutor-d-flex">
-                                    <span class="tutor-icon-level tutor-mt-4"></span>
-                                    <?php /*echo $course['course_level']; */?>
-                                </li>
-                                <li class="tutor-d-flex">
-                                    <span class="tutor-icon-clock-line tutor-mt-4"></span>
-                                    <?php /*echo $course['course_duration']; */?>
-                                </li>
-                            </ul>
-                            <h4>
-                                <a href="<?php /*echo $course['course_url']; */?>" class="link" title="<?php /*echo $course['course_title']; */?>">
-                                    <?php /*echo $course['course_title']; */?>
-                                </a>
-                            </h4>
-                            <p><?php /*echo $course['course_excerpt']; */?></p>
-                        </div>
-                    </div>
-                
-            <?php /*endforeach; */?>
-            </div>
-            
-        </div>
-    </div>
-</section>-->
-<!-- End Course Section -->
-
     <main id="main">
         <div class="container">
             <section id="page-home" class="page-home">
                 <div class="container">
-
                     <?php while (have_posts()) : the_post(); ?>
                         <?php the_content(); ?>
                     <?php endwhile; ?>
@@ -120,7 +71,6 @@ $sliders = tunqui_get_slider();
             </section>
         </div>
     </main><!-- End #main -->
-
 
 <?php
 get_footer();
