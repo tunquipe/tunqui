76c98472dc41436cc5f2ac97151329e7dbc54e21
Alex Aragon <aragcar@gmail.com>
2023-08-15 22:29:46 -0500

    fix menu

--- functions.php
+++ functions.php
@@ -50,11 +50,9 @@ add_action('wp_enqueue_scripts', 'tunqui_register_scripts');
 function tunqui_menus()
 {
     $locations = array(
-        'primary'  => __('Desktop Horizontal Menu', 'tunqui'),
-        'expanded' => __('Desktop Expanded Menu', 'tunqui'),
-        'logged'   => __('Logged', 'tunqui'),
-        'not_logged'   => __('Not Logged', 'tunqui'),
-        'social'   => __('Social Menu', 'tunqui'),
+        'primary'  => __('Menu Header', 'tunqui'),
+        'menu-movil' => __('Menu Movil', 'tunqui'),
+        'menu-footer'   => __('Menu Footer', 'tunqui'),
     );
     register_nav_menus($locations);
 }
