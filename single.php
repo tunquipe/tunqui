def071b3bb41698b636d9ac0d395df0d6365dfda
Alex Aragon <aragcar@gmail.com>
2023-09-01 04:00:40 -0500

    fix blog

--- single.php
+++ single.php
@@ -7,12 +7,17 @@ $idPost = get_the_ID();
 <?php while(have_posts()): the_post(); ?>
     <main id="main">
         <div class="container">
-            <section id="page-single" class="page-single">
-                <div class="page-title">
-                    <h1><?php the_title(); ?></h1>
-                </div>
-                <?php the_content(); ?>
-            </section>
+            <div class="col-md-9">
+                <section id="page-single" class="page-single">
+                    <div class="page-title">
+                        <h1><?php the_title(); ?></h1>
+                    </div>
+                    <?php the_content(); ?>
+                </section>
+            </div>
+            <div class="col-md-3">
+
+            </div>
         </div>
     </main><!-- End #main -->
 <?php endwhile; ?>
